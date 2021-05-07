06c30a1402979c4372392047c56a9bf7d0252367
Petros Tepoyan <petros@tepoyan.ru>
2021-05-07 13:53:07 +0300

    Jokes table view controller fully working

---
+++ InvoltaMockApp/JokeModel.swift
@@ -0,0 +1,32 @@
+//
+//  JokeModel.swift
+//  InvoltaMockApp
+//
+//  Created by Петрос Тепоян on 5/7/21.
+//
+
+import Foundation
+
+protocol JokeModelDelegate {
+	func loadingCompleted()
+}
+
+class JokeModel: NSObject {
+	var jokes: [Joke] = []
+	
+	var delegate: JokeModelDelegate?
+	
+	func loadBatchJokes() {
+		ContentClient.getJokes { result in
+			DispatchQueue.main.async {
+				switch result {
+					case .success(let jokes):
+						self.jokes.append(contentsOf: jokes)
+						self.delegate?.loadingCompleted()
+					case .failure(let error):
+						debugPrint(error)
+				}
+			}
+		}
+	}
+}
--- InvoltaMockApp/JokesTableViewController.swift
+++ InvoltaMockApp/JokesTableViewController.swift
@@ -7,42 +7,43 @@
 
 import UIKit
 
-class JokesTableViewController: UITableViewController {
-
-	var jokes: [Joke] = []
+class JokesTableViewController: UITableViewController,
+								JokeModelDelegate {
+	
+	var jokeModel = JokeModel()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-		ContentClient.getJokes { result in
-			DispatchQueue.main.async {
-				switch result {
-					case .success(let jokes):
-						self.jokes = jokes
-						self.tableView.reloadData()
-					case .failure(let error):
-						debugPrint(error)
-				}
-			}
-		}
+		jokeModel.delegate = self
+		jokeModel.loadBatchJokes()
     }
+	
+	func loadingCompleted() {
+		tableView.reloadData()
+	}
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-		return self.jokes.count
+		return jokeModel.jokes.count
     }
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.jokeCell, for: indexPath) as! JokeTableViewCell
-		cell.configure(joke: jokes[indexPath.row])
+		cell.configure(joke: jokeModel.jokes[indexPath.row])
 		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		print(indexPath.row, jokeModel.jokes.count)
+		guard indexPath.row >= jokeModel.jokes.count - 3 else { return }
+		print("loading jokes...")
+		jokeModel.loadBatchJokes()
 	}
 
 
