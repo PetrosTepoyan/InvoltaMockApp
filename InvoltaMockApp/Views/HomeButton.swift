35b915f21a2e6cf11bf33faa28f38df5cb40e6dc
Petros Tepoyan <petros@tepoyan.ru>
2021-05-07 16:39:04 +0300

    Made buttons squishable

--- InvoltaMockApp/Views/HomeButton.swift
+++ InvoltaMockApp/Views/HomeButton.swift
@@ -9,10 +9,34 @@ import UIKit
 
 class HomeButton: UIButton {
 	
+	private let animator = UIViewPropertyAnimator(duration: 0.3, dampingRatio: 0.5, animations: nil)
+	
 	func configureLayer() {
 		tintColor = .white
 		backgroundColor = UIColor(named: "BlueBackground")
 		layer.cornerRadius = 20
 	}
 	
+	private func squish(identity: Bool = true) {
+		let factor: CGFloat = identity ? 1.0 : 0.9
+		animator.addAnimations {
+			self.transform = CGAffineTransform(scaleX: factor, y: factor)
+		}
+		animator.startAnimation()
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesBegan(touches, with: event)
+		squish(identity: false)
+	}
+	
+	override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesCancelled(touches, with: event)
+		squish()
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesEnded(touches, with: event)
+		squish()
+	}
 }
